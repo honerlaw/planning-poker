4466b5b44497579864bdadba525db30de52921b2
Derek Honerlaw <honerlawd@gmail.com>
2024-08-25 20:57:10 -0400

    use force-dynamic to ensure that the nanoid is generated everytime

--- src/app/room/poker/[id]/layout.tsx
+++ src/app/room/poker/[id]/layout.tsx
@@ -5,6 +5,8 @@ import { nanoid } from "nanoid";
 
 type LayoutProps = React.PropsWithChildren<{ params: { id: string } }>
 
+export const dynamic = 'force-dynamic'
+
 export default function PageLayout({ params, children }: LayoutProps) {
     const userName = useMemoizedRandomName()
     return <FireBaseProvider roomId={params.id} userId={nanoid()} userName={userName}>
--- src/component/LandingPage.tsx
+++ src/component/LandingPage.tsx
@@ -2,6 +2,8 @@ import React from "react"
 import Link from "next/link"
 import { nanoid } from "nanoid"
 
+export const dynamic = 'force-dynamic'
+
 export const LandingPage: React.FC = () => {
     return <div className="flex justify-center items-center h-screen">
         <Link href={`/room/poker/${nanoid()}`}>
