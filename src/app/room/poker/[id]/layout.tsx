da8c5a65ef560035b5d5b83cbf28ae16c68cdb78
Mack Solomon <macksol@gmail.com>
2025-05-22 10:01:43 -0700

    ediable user ids

--- src/app/room/poker/[id]/layout.tsx
+++ src/app/room/poker/[id]/layout.tsx
@@ -1,15 +1,36 @@
+"use client"
+
 import { Header } from "@/component/lib/Header";
 import { FireBaseProvider } from "@/provider/FireBaseProvider";
-import { useMemoizedRandomName } from "@/util/getRandomName";
+import { getUserName, getUserId, setUserId, setUserName } from "@/util/userNameStorage";
 import { nanoid } from "nanoid";
+import { useEffect, useState } from "react";
 
 type LayoutProps = React.PropsWithChildren<{ params: { id: string } }>
 
 export const dynamic = 'force-dynamic'
 
 export default function PageLayout({ params, children }: LayoutProps) {
-    const userName = useMemoizedRandomName()
-    return <FireBaseProvider roomId={params.id} userId={nanoid()} userName={userName}>
+    const [userId, setUserIdState] = useState<string>("");
+    const [userName, setUserName] = useState<string>("");
+
+    useEffect(() => {
+        const storedUserId = getUserId();
+        const newUserId = storedUserId || nanoid();
+        if (!storedUserId) {
+            setUserId(newUserId);
+        }
+        setUserIdState(newUserId);
+
+        const storedUserName = getUserName();
+        setUserName(storedUserName);
+    }, []);
+
+    if (!userId || !userName) {
+        return null;
+    }
+
+    return <FireBaseProvider roomId={params.id} userId={userId} userName={userName}>
         <Header />
         <main>
             {children}
